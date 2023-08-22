8de8c91d7b35853aebb54103f600c663c33a6994
Kunanon Srisuntiroj <son9912@gmail.com>
2023-08-22 22:04:41 +0700

    Refactor server setup and deliver static content
    This commit introduces express.js for handling server setup and routing. Running the server is now handled by a single line in the code 'app.listen(3000)', rather than requiring explicit server creation. An additional middleware function is added to deliver static content from the 'public' folder. Edits include serving pages from '/views' directory than sending manual html, which allows separation of concerns and easy management. Dependency on 'http' module is removed, and process.mainModule.filename is used for dynamic path adjustment. These changes enhance code readability, modularity, and scalability.

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
-const http = require('http');
-const bodyParser = require('body-parser');
+const path = require('path');
+
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use('/', (req,res,next) => {
-  res.status(404).send('<h1>Page not found</h1>');
-})
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
