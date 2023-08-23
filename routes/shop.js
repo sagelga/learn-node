8564a31784e92f11c10a24db6ae294d4326c50e4
Kunanon Srisuntiroj <son9912@gmail.com>
2023-08-23 13:13:34 +0700

    Integrate Pug for dynamic views and update routes
    We've integrated Pug as the templating engine to allow dynamic rendering of views. In consequence, views have been refactored from static HTML files to .pug files. A few changes were made to the server settings to serve Pug views. Routing has been updated to reflect these changes, with data passed dynamically into views and routes rendering Pug files directly. An empty 404.pug file has been established to handle non-existing routes. This commit improves the maintainability and flexibility of code by allowing efficient rendering and organization of views.

--- routes/shop.js
+++ routes/shop.js
@@ -3,11 +3,19 @@ const path = require('path');
 const express = require('express');
 
 const rootDir = require('../util/path');
+const adminData =require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    console.log('shop.js',adminData.products);
+    res.render('shop',
+        {prods: adminData.products,
+            pageTitle: 'Shop',
+            path: '/',
+            hasProducts: adminData.products.length > 0,
+            activeShop: true,
+            productCSS: true})
 });
 
 module.exports = router;
